7b9405b87430bfc571d3a4651509f5e9e6494d96
JOsue Garcia <yoshuah809@gmail.com>
2023-02-10 04:39:30 -0600

    configured router in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddEditBlog from "./pages/AddEditBlog";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="container mt-5">
+    <BrowserRouter>
       <Header />
-      <Form />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addBlog" element={<AddEditBlog />} />
+        <Route path="/editBlog/:id" element={<AddEditBlog />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,31 +2,16 @@ import { MDBInput, MDBTextArea } from "mdb-react-ui-kit";
 
 const Form = () => {
   return (
-    <form className="text-center px-5 my-5 shadow">
-      <div class="form-outline">
+    <form className="d-flex min-vh-50 justify-content-center align-items-center shadow">
+      <div class="form-outline w-50">
         <MDBInput className="mb-3" label="Name" id="form1" type="text" />
-        <MDBInput
-          className="m-3 "
-          label="Password"
-          id="form1"
-          type="password"
-        />
+        <MDBInput className="m-3" label="Password" id="form1" type="password" />
         <MDBTextArea
           className="mb-3"
           id="exampleFormControlTextarea2"
           rows={4}
           label="Input text"
         />
-      </div>
-
-      <div className="form-outline">
-        <MDBTextArea
-          className="mb-3"
-          id="exampleFormControlTextarea1"
-          rows={4}
-          label="Input text"
-        />
-        <br />
       </div>
     </form>
   );
